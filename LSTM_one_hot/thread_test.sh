90a5a310b1bd27337c26f56d14e3b78e2a20359e
dymnz <vicodin1123@gmail.com>
2018-01-05 11:36:29 +0800

    initial result

--- LSTM_one_hot/thread_test.sh
+++ LSTM_one_hot/thread_test.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 thread=$1
-hidden=$3
 while [ "$thread" -le $2 ]
 do 
-
-while [ "$hidden" -le $4 ]
-do
-screen -dmS test$thread ./test.sh $thread $hidden
-while screen -list | grep -q test$thread
-do
-    sleep 1
-done
-hidden=`expr $hidden + 10`
-done
-
+	hidden=$3
+	while [ "$hidden" -le $4 ]
+	do
+		screen -dmS test$thread ./test.sh $thread $hidden
+		while screen -list | grep -q test$thread
+		do
+	    	sleep 1
+		done
+		hidden=`expr $hidden + $5`
+	done
 thread=`expr $thread \* 2`
 done
